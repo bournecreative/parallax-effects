b6f90589f55be35fe14ae98e0ed64527de3eb424
Christian <bournecreative@gmail.com>
2024-06-26 21:11:41 -0500

    import parallax components into index page to demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+import computerSetupOne from '../../public/computer-setup1.jpg'
+import computerSetupTwo from '../../public/computer-setup2.jpg'
+import computerSetupThree from '../../public/computer-setup3.jpg'
+import computerSetupFour from '../../public/computer-setup4.jpg'
+import { ParallaxContainer } from "@/components/parallax-content/ParallaxContainer";
+
 
 export default function Home() {
   return (
     <main>
-      GO
+      <ParallaxContainer img={computerSetupOne} heading="Let's Go" subheading='The best is yet to come.'>
+        
+      </ParallaxContainer>
+      <ParallaxContainer img={computerSetupTwo} heading="Let's Go" subheading='The best is yet to come.'>
+        
+      </ParallaxContainer>
+      <ParallaxContainer img={computerSetupThree} heading="Let's Go" subheading='The best is yet to come.'>
+        
+      </ParallaxContainer>
+      <ParallaxContainer img={computerSetupFour} heading="Let's Go" subheading='The best is yet to come.'>
+        
+      </ParallaxContainer>
     </main>
   );
 }
